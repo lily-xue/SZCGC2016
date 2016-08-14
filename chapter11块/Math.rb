23cee51413738a1a038d91990fb6c9adc8b70aed
lily-xue <414564926@qq.com>
2016-08-14 14:02:11 +0800

    week2——selflf_check

---
+++ chapter11块/Math.rb
@@ -0,0 +1,29 @@
+class Math
+#  def initialize(x,y)
+
+  #end
+def add(x,y)
+  print(x+y)
+end
+
+def subtract(x,y)
+  print(x-y)
+end
+
+
+
+def multiply(x,y)
+   print(x*y)
+end
+  def divide(x,y)
+    if(y)
+    print(x/y+x%y)
+  else
+    print"参数不合法"
+  end
+end
+
+a=ARVG[0]
+b=ARVG[1]
+
+end
